079e2677710fdcfee1479400c64162c06971a3de
malekseychyk3 <malekseychyk3@gatech.edu>
2024-12-05 20:56:48 -0500

    Added Space Mode.

--- main_menu.gd
+++ main_menu.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://DemoScene.tscn")
+	get_tree().change_scene_to_file("res://SpaceDemoScene.tscn")
 
 
 func _on_quit_button_pressed() -> void:
